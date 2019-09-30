781a85ba6dfea995a65627835e02ab6c046cfcf0
JianHMai <42229566+JianHMai@users.noreply.github.com>
2019-09-29 22:16:21 -0400

    Form validation

--- scripts/auth.js
+++ scripts/auth.js
@@ -5,9 +5,42 @@ registerForm.addEventListener("submit", (e)=>{
     // get user info
     const email = registerForm["inputEmail"].value;
     const password = registerForm["inputPassword"].value;
+    const passwordConfirm= registerForm["inputPasswordConfirm"].value;
 
+    let correctPassword;
+    let emailCorrect;
+
+    if(password.length!==passwordConfirm.length){
+        alert("Password Mismatch");
+        correctPassword=false;
+    }
+    else if(password.length<6){
+        alert("Password length must be 6 or greater");
+        correctPassword=false;
+    }
+
+    for(let i = 0; i<email.length; i++){
+        if(email.charAt(i)==='@'){
+            if(email.charAt(i)==='.'){
+                emailCorrect=true;
+                break;
+            }
+        }
+    }
+    if(emailCorrect===false){
+        alert("Invalid Email");
+    }
+
+    for(let i=0; i<password.length; i++){
+        if(password.charAt(i)!==passwordConfirm.charAt(i)){
+            alert("Password Mismatch");
+            correctPassword=false;
+            break;
+        }
+    }
+
+    if(correctPassword && emailCorrect){
     // sign up user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred);
-    })
-})
+        alert(cred);
+    })}})
