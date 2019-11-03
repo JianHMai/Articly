2212f25b1db17896902e20e6ef9c9a2769e5ce28
Taimoor Hafeez <taimoorh5@gmail.com>
2019-11-03 15:37:45 -0500

    updating sources and categories

---
+++ scripts/technology.js
@@ -0,0 +1,38 @@
+var app = document.getElementById('root');
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=8e968903a7034e199d7fa5a223ccefd4', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  var articleList = data.articles;
+
+  articleList.forEach(article => {
+
+    var articleTitle = document.createElement('h1')
+    articleTitle.textContent = article.title;
+
+    var articleDate = document.createElement('p')
+    articleDate.textContent = article.publishedAt;
+
+    var articleDescription = document.createElement('p')
+    articleDescription.textContent = article.description;
+
+    var articleURL = document.createElement('a');
+    articleURL.textContent = 'Link to Article';
+    articleURL.href = article.url;
+
+    var articleImage = document.createElement('img');
+    articleImage.src = article.urlToImage;
+
+    app.appendChild(articleTitle);
+    app.appendChild(articleDate);
+    app.appendChild(articleImage);
+    app.appendChild(articleDescription);
+    app.appendChild(articleURL);
+
+    });
+  } 
+
+request.send();
