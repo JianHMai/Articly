388a25c1bd863df5d73454e494998e5ede1ee81d
Jason Yang <jasonyang1496@gmail.com>
2019-11-10 22:34:51 -0500

    Add sign out and log in functions

--- scripts/auth.js
+++ scripts/auth.js
@@ -10,13 +10,13 @@ registerForm.addEventListener("submit", (e)=>{
     let correctPassword=true;
     let correctEmail=true;
 
-    if(email.length===0){
+    if(email.length===0){ // check if email field is empty
         alert("Email field empty");
         correctEmail=false;
     }
 
     if(password.length!==passwordConfirm.length){
-        alert("Password Mismatch");
+        alert("Password mismatch");
         correctPassword=false;
     }
     else if(password.length<6){
@@ -34,17 +34,47 @@ registerForm.addEventListener("submit", (e)=>{
 
     for(let i=0; i<password.length; i++){
         if(password.charAt(i)!==passwordConfirm.charAt(i)){
-            alert("Password Mismatch");
+            alert("Password mismatch");
             correctPassword=false;
             break;
         }
     }
 
     if(correctPassword && correctEmail){
-    // sign up user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        alert("Account created successfully");
-        window.location.href="login.html";
-    }).catch(function (error) {
-        alert(error);
-    })}});
+        // sign up user
+        auth.createUserWithEmailAndPassword(email, password).then(cred => {
+            console.log(cred.user);
+            alert("Account created successfully");
+            window.location.href="login.html";
+        }).catch(function (error) {
+            alert(error);
+        })
+    }
+});
+
+// sign out
+const signout = document.querySelector('#signout');
+signout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("user signed out");
+        alert("User signed out");
+        window.location.href="index.html";
+    })
+})
+
+// log in
+const logInForm = document.querySelector("#logInForm");
+logInForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = logInForm['logInEmail'].value;
+    const password = logInForm['logInPassword'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        alert("User signed in");
+        window.location.href="index.html";
+    })
+})
