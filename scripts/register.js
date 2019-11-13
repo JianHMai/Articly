ac616002fa643708e5ca1c1910bd6f2715fc4874
JianHMai <42229566+JianHMai@users.noreply.github.com>
2019-11-13 18:36:05 -0500

    Working log in and register

--- scripts/register.js
+++ scripts/register.js
@@ -1,22 +1,18 @@
 const registerForm = document.querySelector("#registerForm");
 registerForm.addEventListener("submit", (e)=>{
     e.preventDefault();
-
     // get user info
     const email = registerForm["inputEmail"].value;
     const password = registerForm["inputPassword"].value;
     const passwordConfirm= registerForm["inputPasswordConfirm"].value;
-
     let correctPassword=true;
     let correctEmail=true;
-
-    if(email.length===0){ // check if email field is empty
+    if(email.length===0){
         alert("Email field empty");
         correctEmail=false;
     }
-
     if(password.length!==passwordConfirm.length){
-        alert("Password mismatch");
+        alert("Password Mismatch");
         correctPassword=false;
     }
     else if(password.length<6){
@@ -31,50 +27,18 @@ registerForm.addEventListener("submit", (e)=>{
         alert("Password confirmation field blank");
         correctPassword=false;
     }
-
     for(let i=0; i<password.length; i++){
         if(password.charAt(i)!==passwordConfirm.charAt(i)){
-            alert("Password mismatch");
+            alert("Password Mismatch");
             correctPassword=false;
             break;
         }
     }
-
     if(correctPassword && correctEmail){
         // sign up user
         auth.createUserWithEmailAndPassword(email, password).then(cred => {
-            console.log(cred.user);
             alert("Account created successfully");
-            window.location.href="login.html";
+            window.location.href="index.html";
         }).catch(function (error) {
             alert(error);
-        })
-    }
-});
-
-// sign out
-const signout = document.querySelector('#signout');
-signout.addEventListener('click', (e) => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-        console.log("user signed out");
-        alert("User signed out");
-        window.location.href="index.html";
-    })
-})
-
-// log in
-const logInForm = document.querySelector("#logInForm");
-logInForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // get user info
-    const email = logInForm['logInEmail'].value;
-    const password = logInForm['logInPassword'].value;
-
-    auth.signInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
-        alert("User signed in");
-        window.location.href="index.html";
-    })
-})
+})}});
---
+++ scripts/signout.js
@@ -0,0 +1,10 @@
+// sign out
+const signout = document.querySelector('#signout');
+signout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("user signed out");
+        alert("User signed out");
+        window.location.href="index.html";
+    })
+});
