68dc539ae6a3d0b03cd1065edf5a98fb9d70205c
JianHMai <42229566+JianHMai@users.noreply.github.com>
2019-11-20 21:32:40 -0500

    Replace The Guardian with Associated Press

---
+++ scripts/associatedpress.js
@@ -0,0 +1,38 @@
+let app = document.getElementById('root');
+
+let request = new XMLHttpRequest();
+request.open('GET', 'https://newsapi.org/v2/top-headlines?sources=associated-press&apiKey=8e968903a7034e199d7fa5a223ccefd4', true);
+request.onload = function () {
+
+    // Begin accessing JSON data here
+    let data = JSON.parse(this.response);
+    let articleList = data.articles;
+
+    articleList.forEach(article => {
+
+        let articleTitle = document.createElement('h1');
+        articleTitle.textContent = article.title;
+
+        let articleDate = document.createElement('p');
+        articleDate.textContent = article.publishedAt;
+
+        let articleDescription = document.createElement('p');
+        articleDescription.textContent = article.description;
+
+        let articleURL = document.createElement('a');
+        articleURL.textContent = 'Link to Article';
+        articleURL.href = article.url;
+
+        let articleImage = document.createElement('img');
+        articleImage.src = article.urlToImage;
+
+        app.appendChild(articleTitle);
+        app.appendChild(articleDate);
+        app.appendChild(articleImage);
+        app.appendChild(articleDescription);
+        app.appendChild(articleURL);
+
+    });
+};
+
+request.send();
