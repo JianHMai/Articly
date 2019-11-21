6ecbcc5d2af2dc9ae9de616282216b71b4d103d4
JianHMai <42229566+JianHMai@users.noreply.github.com>
2019-11-20 20:53:51 -0500

    Add a replace + with a - to enable api compatibility

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,5 @@
 let parameters=location.search.substring(1).split("&");
 let temp = parameters[0].split("=");
 let l=unescape(temp[1]);
+let searchValue= '+';
+let str = l.replace(searchValue, "-");
