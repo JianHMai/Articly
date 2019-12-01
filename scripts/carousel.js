0a0f587ea4107c18af13d9e89ecdad0ce4c7048f
JianHMai <42229566+JianHMai@users.noreply.github.com>
2019-12-01 18:45:06 -0500

    Update link color of carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -5,15 +5,20 @@ $(document).ready(function () {
         // Begin accessing JSON data here
         let data = JSON.parse(this.response);
 
+        let author = "";
+
         for (let i = 0; i < 3; i++) {
+            if (data.articles[i].author === null) {
+                author = "";
+            } else author = data.articles[i].author;
             $('<div class="carousel-item"><img class="img-fluid" src="' + data.articles[i].urlToImage + '" style= ' +
                 '"width: 60vw; height: 30vw;"><div class="carousel-caption"><h5><a href=' + data.articles[i].url +
-             '>' + data.articles[i].title + '</a></h5><p>' + data.articles[i].author +'</p></div> </div>').appendTo('.carousel-inner');
+                ' style = "color:black">' + data.articles[i].title + '</a></h5><p>' + author + '</p></div> </div>').appendTo('.carousel-inner');
             $('<li data-target="#carousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators');
         }
-    $('.carousel-item').first().addClass('active');
-    $('.carousel-indicators > li').first().addClass('active');
-    $('#carousel').carousel();
-};
-request.send();
+        $('.carousel-item').first().addClass('active');
+        $('.carousel-indicators > li').first().addClass('active');
+        $('#carousel').carousel();
+    };
+    request.send();
 });
