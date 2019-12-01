27f1661c476a168f9ed30db273ab4a952fb57005
taimoorh5 <taimoorh5@gmail.com>
2019-11-30 23:03:03 -0500

    updating design a bit

--- scripts/abcNews.js
+++ scripts/abcNews.js
@@ -1,5 +1,10 @@
 let app = document.getElementById('root');
 
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(container);
+
 let request = new XMLHttpRequest();
 request.open('GET', 'https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=8e968903a7034e199d7fa5a223ccefd4', true);
 request.onload = function () {
@@ -10,6 +15,9 @@ request.onload = function () {
 
     articleList.forEach(article => {
 
+        const card = document.createElement('div');
+        card.setAttribute('class', "card");
+
         let articleTitle = document.createElement('h1');
         articleTitle.textContent = article.title;
 
@@ -18,19 +26,21 @@ request.onload = function () {
 
         let articleDescription = document.createElement('p');
         articleDescription.textContent = article.description;
+        
+        let articleImage = document.createElement('img');
+        articleImage.src = article.urlToImage;
 
         let articleURL = document.createElement('a');
         articleURL.textContent = 'Link to Article';
         articleURL.href = article.url;
 
-        let articleImage = document.createElement('img');
-        articleImage.src = article.urlToImage;
+        container.appendChild(card);
 
-        app.appendChild(articleTitle);
-        app.appendChild(articleDate);
-        app.appendChild(articleImage);
-        app.appendChild(articleDescription);
-        app.appendChild(articleURL);
+        card.appendChild(articleTitle);
+        card.appendChild(articleDate);
+        card.appendChild(articleImage);
+        card.appendChild(articleDescription);
+        card.appendChild(articleURL);
 
     });
 };
