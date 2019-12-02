d34dbb826629bf48d7f8886adfbf5f2906e386df
JianHMai <42229566+JianHMai@users.noreply.github.com>
2019-12-01 19:08:48 -0500

    Add placeholder for null images

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -6,12 +6,16 @@ $(document).ready(function () {
         let data = JSON.parse(this.response);
 
         let author = "";
+        let image = "";
 
         for (let i = 0; i < 3; i++) {
             if (data.articles[i].author === null) {
                 author = "";
             } else author = data.articles[i].author;
-            $('<div class="carousel-item"><img class="img-fluid" src="' + data.articles[i].urlToImage + '" style= ' +
+            if (data.articles[i].urlToImage === null) {
+                image = "images/noImage.png"
+            } else image = data.articles[i].urlToImage;
+            $('<div class="carousel-item"><img class="img-fluid" src="' + image + '" style= ' +
                 '"width: 60vw; height: 30vw;"><div class="carousel-caption"><h5><a href=' + data.articles[i].url +
                 ' style = "color:black">' + data.articles[i].title + '</a></h5><p>' + author + '</p></div> </div>').appendTo('.carousel-inner');
             $('<li data-target="#carousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators');
