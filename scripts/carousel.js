1e3b11f396f05c875f891eb543b3030d50bc4a65
Jian Mai <42229566+JianHMai@users.noreply.github.com>
2020-07-01 19:50:43 -0400

    reverse api update

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     let request = new XMLHttpRequest();
-    request.open('GET', 'http://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=8e968903a7034e199d7fa5a223ccefd4');
+    request.open('GET', 'https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=8e968903a7034e199d7fa5a223ccefd4');
     request.onload = function () {
         // Begin accessing JSON data here
         let data = JSON.parse(this.response);
